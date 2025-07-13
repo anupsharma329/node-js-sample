b4d1150434c2e16dc3b9210068713dfd5263a5c4
anupsharma329 <anupsharma329@gmail.com>
2025-07-13 19:46:12 +0530

    new changes for checking

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ app.set('port', (process.env.PORT || 3000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('This is my sample project - Hello World!')
 })
 
 app.listen(app.get('port'), '0.0.0.0', function() {
   console.log("Node app is running at 0.0.0.0:" + app.get('port'))
 })
+console.log("🚀 Updated at " + new Date());
